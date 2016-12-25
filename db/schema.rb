485c73b2526a2918e6930f1a1fab5e66f9d5d579
yamamotomakiko <yamamoto-makiko@av.avex.co.jp>
2016-12-25 07:05:26 +0000

    実習08

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219112602) do
+ActiveRecord::Schema.define(version: 20161225070342) do
+
+  create_table "timelines", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false

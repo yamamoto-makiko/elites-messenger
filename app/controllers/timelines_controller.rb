5c964aad139143372187e316a750dcefc60e3932
yamamotomakiko <yamamoto-makiko@av.avex.co.jp>
2016-12-25 07:51:52 +0000

    実習10

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -2,7 +2,9 @@ class TimelinesController < ApplicationController
   def index
     # メッセージ入力
     #--******************** 下記を追加 *******************
-    @input_message = Timeline.new
+    # @input_message = Timeline.new
+    # メッセージ入力
+    @input_message = params[:id] ? Timeline.find(params[:id]) : Timeline.new
     #--***************************************************
     #--************************** 下記を追加 *********************
     # タイムラインを取得
@@ -14,6 +16,17 @@ class TimelinesController < ApplicationController
     timeline = Timeline.new
     timeline.attributes = input_message_param
     timeline.user_id = current_user.id
+    if timeline.valid? # バリデーションチェック
+      timeline.save!
+    else
+      flash[:alert] = timeline.errors.full_messages
+    end
+    redirect_to action: :index
+  end
+
+  def update
+    timeline = Timeline.find(params[:id])
+    timeline.attributes = input_message_param
     if timeline.valid? # バリデーションチェック
       timeline.save!
     else
